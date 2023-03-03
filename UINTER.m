0b63cd1e65b1f34b170c11479f403d710f146f17
mklupar <88486168+mklupar@users.noreply.github.com>
2023-03-03 11:26:13 -0600

    Update UINTER.m
    Update XL to right size

--- UINTER.m
+++ UINTER.m
@@ -2,12 +2,14 @@
 % DOUBLE CHECK THIS 
 function UIN = UINTER(XL,YL,XIN,YIN,UB,UNB,IN)
 N = length(XL);
+XL(N+1) = XL(1);
+YL(N+1) = YL(1);
     for k = 1:IN
         UIN(k) = 0;
         for j = 1:N 
             JP1 = j+1;
-            DALPHA(XIN(k),YIN(k),XL(j),YL(j),XL(JP1),YL(JP1),RESH)
-            RLINTC(XIN(k),YIN(k),XL(j),YL(j),XL(JP1),YL(JP1),RESG)
+            RESH = DALPHA(XIN(k),YIN(k),XL(j),YL(j),XL(JP1),YL(JP1))
+            RESG = RLINTC(XIN(k),YIN(k),XL(j),YL(j),XL(JP1),YL(JP1))
             UIN(K) = UIN(K) + RESH*UB(j) - RESG*UNB(j);
         end
     end
